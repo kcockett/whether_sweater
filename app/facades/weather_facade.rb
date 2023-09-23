a891e1d2444bb83337703fbf64569c42112cbf30
Kaina Cockett <kainacockett@gmail.com>
2023-09-23 11:01:39 -0600

    feat: create Weather Facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -8,7 +8,6 @@ class WeatherFacade
   def current_weather
     service = WeatherService.new(@location)
     weather_info = service.get_current_weather
-    require 'pry'; binding.pry
     Weather.new(weather_info)
   end
 end
--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -13,21 +13,21 @@ RSpec.describe "WeatherFacade", :vcr, type: :facade do
 
       expect(weather_check).to be_a(Weather)
       expect(weather_check.location[:name]).to eq("Thornton")
-      expect(weather_check.location[:region]).to eq("Colorado")
+      expect(weather_check.location[:state]).to eq("Colorado")
       expect(weather_check.location[:country]).to eq("United States of America")
       expect(weather_check.location[:lat]).to eq(39.87)
       expect(weather_check.location[:lon]).to eq(-104.97)
-      expect(weather_check.location[:tz_id]).to eq("America/Denver")
+      expect(weather_check.location[:time_zone]).to eq("America/Denver")
       expect(weather_check.location[:localtime]).to be_a(String)
       
       expect(weather_check.current[:last_updated]).to be_a(String)
       expect(weather_check.current[:temp]).to be_a(Float)
-      expect(weather_check.current[:feelslike_f]).to be_a(Float)
+      expect(weather_check.current[:feels_like]).to be_a(Float)
       expect(weather_check.current[:humidity]).to be_a(Integer)
-      expect(weather_check.current[:uv]).to be_a(Float)
-      expect(weather_check.current[:vis_miles]).to be_a(Float)
-      expect(weather_check.current[:condition][:text]).to be_a(String)
-      expect(weather_check.current[:condition][:icon]).to be_a(String)
+      expect(weather_check.current[:uvi]).to be_a(Float)
+      expect(weather_check.current[:visibility]).to be_a(Float)
+      expect(weather_check.current[:condition]).to be_a(String)
+      expect(weather_check.current[:icon]).to be_a(String)
     end
   end
 end
