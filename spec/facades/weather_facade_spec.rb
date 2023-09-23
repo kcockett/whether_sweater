2688868c34c4015543795fa2898dc75ad3496095
Kaina Cockett <kainacockett@gmail.com>
2023-09-23 11:25:43 -0600

    test: 5-day forecast added to Weather object

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -10,31 +10,39 @@ RSpec.describe "WeatherFacade", :vcr, type: :facade do
     end
 
     it "returns a Weather object with location information" do
+      info = @weather_check.location
       expect(@weather_check).to be_a(Weather)
-      expect(@weather_check.location[:name]).to eq("Thornton")
-      expect(@weather_check.location[:state]).to eq("Colorado")
-      expect(@weather_check.location[:country]).to eq("United States of America")
-      expect(@weather_check.location[:lat]).to eq(39.87)
-      expect(@weather_check.location[:lon]).to eq(-104.97)
-      expect(@weather_check.location[:time_zone]).to eq("America/Denver")
-      expect(@weather_check.location[:localtime]).to be_a(String)
+      expect(info[:name]).to eq("Thornton")
+      expect(info[:state]).to eq("Colorado")
+      expect(info[:country]).to eq("United States of America")
+      expect(info[:lat]).to eq(39.87)
+      expect(info[:lon]).to eq(-104.97)
+      expect(info[:time_zone]).to eq("America/Denver")
+      expect(info[:localtime]).to be_a(String)
     end
-
-    it "returns a Weather object with current weather information" do
-      expect(@weather_check.current[:last_updated]).to be_a(String)
-      expect(@weather_check.current[:temp]).to be_a(Float)
-      expect(@weather_check.current[:feels_like]).to be_a(Float)
-      expect(@weather_check.current[:humidity]).to be_a(Integer)
-      expect(@weather_check.current[:uvi]).to be_a(Float)
-      expect(@weather_check.current[:visibility]).to be_a(Float)
-      expect(@weather_check.current[:condition]).to be_a(String)
-      expect(@weather_check.current[:icon]).to be_a(String)
+    
+    it "the Weather object includes current weather information" do
+      info = @weather_check.current
+      expect(info[:last_updated]).to be_a(String)
+      expect(info[:temp]).to be_a(Float)
+      expect(info[:feels_like]).to be_a(Float)
+      expect(info[:humidity]).to be_a(Integer)
+      expect(info[:uvi]).to be_a(Float)
+      expect(info[:visibility]).to be_a(Float)
+      expect(info[:condition]).to be_a(String)
+      expect(info[:icon]).to be_a(String)
     end
-
-    it "returns a 5-day forecast in the same Weather object" do
-      
-
-
+    
+    it "the Weather object includes a 5-day weather forecast" do
+      forecast_day = @weather_check.forecast.first
+      expect(forecast_day[:date]).to be_a(String)
+      expect(forecast_day[:sunrise]).to be_a(String)
+      expect(forecast_day[:sunset]).to be_a(String)
+      expect(forecast_day[:max_temp]).to be_a(Float)
+      expect(forecast_day[:min_temp]).to be_a(Float)
+      expect(forecast_day[:condition]).to be_a(String)
+      expect(forecast_day[:icon]).to be_a(String)
     end
+  
   end
 end
