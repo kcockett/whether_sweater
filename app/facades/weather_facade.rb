9bd5bc2a2be0ed84f37141c5d06f99ca2c3cc174
Kaina Cockett <kainacockett@gmail.com>
2023-09-23 18:26:26 -0600

    wip: conversion to Mapquest location

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,8 +1,11 @@
 class WeatherFacade
-  attr_reader
+  attr_reader :location, :weather
 
   def initialize(location)
-    @location = location
+    latitude = MapquestService.new(location).location[:results].first[:locations].first[:latLng][:lat]
+    longitude = MapquestService.new(location).location[:results].first[:locations].first[:latLng][:lng]
+    @location = "#{latitude},#{longitude}"
+    @weather = get_weather
   end
 
   def get_weather
