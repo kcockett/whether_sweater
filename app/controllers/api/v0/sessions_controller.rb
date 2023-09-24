68e8a12e6687ea4379b45b5099979c84819a1928
Kaina Cockett <kainacockett@gmail.com>
2023-09-24 16:46:56 -0600

    test: sessions sad path

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,14 +1,14 @@
 class Api::V0::SessionsController < ApplicationController
   def create
     if !user_params[:email] || !user_params[:password]
-      render json: ErrorSerializer.serialize("Invalid credentials"), status: 401
+      render json: ErrorSerializer.format_errors("Invalid credentials"), status: 401
     else
       if !User.find_by(email: user_params[:email])
-        render json: ErrorSerializer.serialize("Invalid credentials"), status: 401
+        render json: ErrorSerializer.format_errors("Invalid credentials"), status: 401
       else
         user = User.find_by(email: user_params[:email])
         if !user.authenticate(user_params[:password])
-          render json: ErrorSerializer.serialize("Invalid credentials"), status: 401
+          render json: ErrorSerializer.format_errors("Invalid credentials"), status: 401
         else
           render json: UserSerializer.new(user)
         end
--- spec/requests/api/v0/sessions_spec.rb
+++ spec/requests/api/v0/sessions_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Sessions request" do
         expect(reply[:data][:type]).to eq("users")
 
         expect(reply[:data]).to have_key(:id)
-        expect(reply[:data][:id]).to eq("1")
+        expect(reply[:data][:id]).to eq(user.id.to_s)
 
         expect(reply[:data]).to have_key(:attributes)
         expect(reply[:data][:attributes]).to be_a Hash
@@ -34,6 +34,79 @@ RSpec.describe "Sessions request" do
         
         expect(reply[:data][:attributes]).to have_key(:api_key)
         expect(reply[:data][:attributes][:api_key]).to eq(user.api_key)
+    end
+
+    describe "SAD PATHS:  User tries to login with missing or incorrect information" do
+
+      before do
+        @user = User.create!(email: "user@example.com", password: "password")
+      end
+
+      it "Logging in with missing email renders a json response with an 'Invalid parameters' error" do
+        
+        json_payload = {
+          # email: @user.email,
+          password: "password"
+        }.to_json
+  
+        post '/api/v0/sessions',
+          params: json_payload,
+          headers: {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+          }
+        reply = JSON.parse(response.body, symbolize_names: true)
+  
+        expect(response.status).to eq(401)
+        expect(reply).to have_key(:errors)
+        expect(reply[:errors]).to be_a Array
+        expect(reply[:errors].first).to have_key(:detail)
+        expect(reply[:errors].first[:detail]).to eq("Invalid credentials")
+      end
+
+      it "Logging in with missing password renders a json response with an 'Invalid parameters' error" do
+        
+        json_payload = {
+          email: @user.email,
+          # password: "password"
+        }.to_json
+  
+        post '/api/v0/sessions',
+          params: json_payload,
+          headers: {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+          }
+        reply = JSON.parse(response.body, symbolize_names: true)
+  
+        expect(response.status).to eq(401)
+        expect(reply).to have_key(:errors)
+        expect(reply[:errors]).to be_a Array
+        expect(reply[:errors].first).to have_key(:detail)
+        expect(reply[:errors].first[:detail]).to eq("Invalid credentials")
+      end
+
+      it "Logging in with incorrect password renders a json response with an 'Invalid parameters' error" do
+        
+        json_payload = {
+          email: @user.email,
+          password: "wrong_password"
+        }.to_json
+  
+        post '/api/v0/sessions',
+          params: json_payload,
+          headers: {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+          }
+        reply = JSON.parse(response.body, symbolize_names: true)
+  
+        expect(response.status).to eq(401)
+        expect(reply).to have_key(:errors)
+        expect(reply[:errors]).to be_a Array
+        expect(reply[:errors].first).to have_key(:detail)
+        expect(reply[:errors].first[:detail]).to eq("Invalid credentials")
+      end
     end
   end
 
