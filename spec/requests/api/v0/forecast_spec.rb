2717eabb1de84017617bcaacc3803493e77a6dcc
Kaina Cockett <kainacockett@gmail.com>
2023-09-24 11:27:55 -0600

    test: has key daily_weather that is an array

--- spec/requests/api/v0/forecast_spec.rb
+++ spec/requests/api/v0/forecast_spec.rb
@@ -86,38 +86,45 @@ RSpec.describe "Forecast API", type: :request do
           expect(@response[:data][:attributes][:current_weather]).to have_key(:icon)
           expect(@response[:data][:attributes][:current_weather][:icon]).to be_a(String)
         end
-
       end
+      
+      describe "the object holds 'daily_weather' with weather data for the next 5 days with the following: " do
+
+        it "should have key 'daily_weather' that is an array" do
+          expect(@response[:data][:attributes]).to have_key(:daily_weather)
+          expect(@response[:data][:attributes][:daily_weather]).to be_a(Array)
+        end
+        
+        # it "date, in a human-readable format such as '2023-04-07'" do
+          
+        # end
+        
+        # it "sunrise, in a human-readable format such as '07:13 AM'"
+        
+        # it "sunset, in a human-readable format such as “08:07 PM”"
+        
+        # it "max_temp, floating point number indicating the maximum expected temperature in Fahrenheit"
+        
+        # it "min_temp, floating point number indicating the minimum expected temperature in Fahrenheit"
+        
+        # it "condition, the text description for the weather condition"
+        
+        # it "icon, png string for weather condition"
+        
+      end
+      
+      #   scenario "the object holds 'hourly_weather' which is an array of all 24 hour’s hour data for the current day, including:" do
+      
+      #     it "time, in a human-readable format such as “22:00”"
+      
+      #     it "temperature, floating point number indicating the temperature in Fahrenheit for that hour"
+      
+      #     it "conditions, the text description for the weather condition at that hour"
+      
+      #     it "icon, string, png string for weather condition at that hour"
+      
+      #   end
+      # end
     end
-    #   scenario "the object holds 'daily_weather' with weather data for the next 5 days with the following: " do
-
-    #     it "date, in a human-readable format such as '2023-04-07'"
-
-    #     it "sunrise, in a human-readable format such as '07:13 AM'"
-
-    #     it "sunset, in a human-readable format such as “08:07 PM”"
-
-    #     it "max_temp, floating point number indicating the maximum expected temperature in Fahrenheit"
-
-    #     it "min_temp, floating point number indicating the minimum expected temperature in Fahrenheit"
-
-    #     it "condition, the text description for the weather condition"
-
-    #     it "icon, png string for weather condition"
-
-    #   end
-
-    #   scenario "the object holds 'hourly_weather' which is an array of all 24 hour’s hour data for the current day, including:" do
-
-    #     it "time, in a human-readable format such as “22:00”"
-
-    #     it "temperature, floating point number indicating the temperature in Fahrenheit for that hour"
-
-    #     it "conditions, the text description for the weather condition at that hour"
-
-    #     it "icon, string, png string for weather condition at that hour"
-
-    #   end
-    # end
   end
 end
