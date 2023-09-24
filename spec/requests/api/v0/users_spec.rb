80a071129423de837d202474399217c35c5035a4
Kaina Cockett <kainacockett@gmail.com>
2023-09-24 14:03:00 -0600

    test for successful add response

---
+++ spec/requests/api/v0/users_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Users API", type: :request do
+  describe "POST /api/v0/users", :vcr do
+    
+    describe "when making a valid request (POST /api/v0/users), the response should return a specific set of data in JSON format" do
+      it "should return a status of 201" do
+        json_payload = {
+          email: "my_email@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }.to_json
+
+        post '/api/v0/users',
+          params: json_payload,
+          headers: {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+          }
+
+        expect(response.status).to eq(201)
+      end
+    end
