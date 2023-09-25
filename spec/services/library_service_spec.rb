06ee70b1ba7c5111a7009b1c216629600496287d
Kaina Cockett <kainacockett@gmail.com>
2023-09-25 09:38:52 -0600

    test: first test created for service

--- spec/services/library_service_spec.rb
+++ spec/services/library_service_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
-RSpec.describe "Open Library Service", type: :service do
+RSpec.describe "Library Service", type: :service do
   describe "Retrieves location information from https://openlibrary.rog/search.json?q=[title]&limit=[max number returned]", :vcr do
 
     before do
-      params = {q: "Denver, Colorado", limit: 3}
+      @location = "Denver, Colorado"
+      @limit = 3
+      params = {q: @location, limit: @limit}
       service = OpenLibraryService.new(params)
       @book_info = service.book_list
     end
@@ -17,6 +19,13 @@ RSpec.describe "Open Library Service", type: :service do
       
       expect(@book_info).to have_key("docs")
       expect(@book_info["docs"]).to be_a Array
+      expect(@book_info["docs"].count).to be_a eq(@limit)
+
+      expect(@book_info["docs"].first).to have_key("title")
+      expect(@book_info["docs"].first["title"]).to be_a String
+
+      expect(@book_info["docs"].first).to have_key("isbn")
+      expect(@book_info["docs"].first["isbn"]).to be_a Array
     end
   end
 end
